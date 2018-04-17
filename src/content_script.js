5f8775bad86b733e1b69534d72bc069a2392e77e
Jim Horvath <jhorv@users.noreply.github.com>
2018-04-17 11:11:41 -0400

    improve exception handling when accessing the sheet.rules property

--- src/content_script.js
+++ src/content_script.js
@@ -2,32 +2,35 @@
     'use strict';
     var DEFAULT_DESIRED_SCROLLBAR_WIDTH = 20;
     var desiredScrollbarWidth = DEFAULT_DESIRED_SCROLLBAR_WIDTH;
-
     function escapeRegExp(str) {
         return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
     }
+    var selectorRegex = new RegExp(escapeRegExp('#wunderlist-base ::-webkit-scrollbar'));
+    var widthRegex = /width:/;    
 
     function doWork() {
         console.log("WSE: starting wunderlist scrollbar width fix");
-        var selectorRegex = new RegExp(escapeRegExp('#wunderlist-base ::-webkit-scrollbar'));
-        var widthRegex = /width:/;
-
         var isSuccessful = false;
-
         try {
             for (var styleSheetIndex = 0; styleSheetIndex < document.styleSheets.length; styleSheetIndex++) {
                 var sheet = document.styleSheets[styleSheetIndex];
-                if (sheet.rules) {
-                    //console.log("rules: " + sheet.rules.length);
-                    for (var i = 0; i < sheet.rules.length; ++i) {
-                        var rule = sheet.rules[i];
-                        if (selectorRegex.test(rule.cssText) && widthRegex.test(rule.cssText)) {
-                            console.log("WSE: found scrollbar width style: " + rule.cssText);
-                            sheet.deleteRule(i);
-                            sheet.insertRule('#wunderlist-base ::-webkit-scrollbar { width: ' + desiredScrollbarWidth + 'px !important; }', i);
-                            isSuccessful = true;
+                try {
+                    if (sheet.rules) {
+                    //if (sheet.hasOwnProperty('rules')) {
+                        //console.log("rules: " + sheet.rules.length);
+                        for (var i = 0; i < sheet.rules.length; ++i) {
+                            var rule = sheet.rules[i];
+                            if (selectorRegex.test(rule.cssText) && widthRegex.test(rule.cssText)) {
+                                console.log("WSE: found scrollbar width style: " + rule.cssText);
+                                sheet.deleteRule(i);
+                                sheet.insertRule('#wunderlist-base ::-webkit-scrollbar { width: ' + desiredScrollbarWidth + 'px !important; }', i);
+                                isSuccessful = true;
+                            }
                         }
                     }
+                }
+                catch (sheetRulesAccessErr) {
+                    console.log(sheetRulesAccessErr);
                 }
             }
         }
